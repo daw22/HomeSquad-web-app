66b695eeddafb95df612d43a5d746eaeb09afa78
daw22 <dawitxxd@gmail.com>
2024-07-07 11:44:15 +0300

    loading spinner and logout fix

--- frontend/src/components/navBar.jsx
+++ frontend/src/components/navBar.jsx
@@ -3,13 +3,14 @@ import { AppBar, Toolbar, Typography, Box, Button } from "@mui/material";
 import FlexBetween from "./flexBetween.jsx";
 import { userContext } from "../context/userContext.jsx";
 import { useNavigate } from "react-router-dom";
+import instance from "../axios.js";
 
 function NavBar() {
   const status = useContext(userContext);
   const navigate = useNavigate();
 
   async function logout() {
-    const response = await fetch("http://localhost:5000/logout");
+    const response = await instance.get("/logout");
     if (!response.ok) return;
     status.setUser(null);
     navigate("/");
