771bc85f2c103de34206517f676167d4def25e52
daw22 <dawitxxd@gmail.com>
2024-07-16 00:51:06 +0300

    frontend final build backend url fix

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const instance = axios.create({
   withCredentials: true,
-  //baseURL: 'https://api-homesquad.onrender.com/'
-  baseURL: 'http://localhost:5000/'
+  baseURL: 'https://api-homesquad.onrender.com/'
+  //baseURL: 'http://localhost:5000/'
 });
 
 export default instance;
